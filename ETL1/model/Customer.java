f2079cc4e3a7d70050d2565097f7f5014f163f94
Kseniya Veka <73733574+vekaonelove@users.noreply.github.com>
2024-06-09 15:56:51 +0300

    Add files via upload

---
+++ ETL1/model/Customer.java
@@ -0,0 +1,63 @@
+package com.example.sqlspringbatch.ETL1.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Customer", uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "surname"})})
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    private String phoneNumber;
+    private String email;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
+
